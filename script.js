70f4bff53f864188b15c9f3bfdef12909d2d0d0c
Ferris <55517321+LivingLIEfe@users.noreply.github.com>
2024-03-26 23:58:39 -0500

    Cannot get javascript to display on the document

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let randomNumber = Number(Math.trunc(Math.random() * 6) + 1);
+`use strict`;
+//Script for console
+
+// let randomNumber = Number(Math.trunc(Math.random() * 6) + 1);
+// let score = 0;
+// let highScore = 0;
 
 const getComputerChoice = function () {
   randomNumber = Number(Math.trunc(Math.random() * 6) + 1);
@@ -11,44 +16,102 @@ const getComputerChoice = function () {
   }
 };
 
-let computerChoice = getComputerChoice();
+// const gameRound = function (playerChoice, computerChoice) {
+//   if (playerChoice === `rock` && computerChoice === `paper`) {
+//     return `lose`;
+//   } else if (playerChoice === `rock` && computerChoice === `scissor`) {
+//     score++;
+//     if (score > highScore) {
+//       highScore = score;
+//     }
+//     return `win`;
+//   } else if (playerChoice === `paper` && computerChoice === `scissor`) {
+//     return `lose`;
+//   } else if (playerChoice === `paper` && computerChoice === `rock`) {
+//     score++;
+//     if (score > highScore) {
+//       highScore = score;
+//     }
+//     return `win`;
+//   } else if (playerChoice === `scissor` && computerChoice === `rock`) {
+//     return `lose`;
+//   } else if (playerChoice === `scissor` && computerChoice === `paper`) {
+//     score++;
+//     if (score > highScore) {
+//       highScore = score;
+//     }
+//     return `win`;
+//   } else {
+//     return `tie`;
+//   }
+// };
 
-let playerChoice = prompt(`Pick rock paper scissors`).toLowerCase();
+// const gamePlay = function () {
+//   for (i = 0; i < 20; i++) {
+//     const playerChoice = prompt(`rock paper or scissors`);
+//     const computerChoice = getComputerChoice();
+//     const result = gameRound(playerChoice, computerChoice);
+//     console.log(playerChoice);
+//     console.log(computerChoice);
+//     console.log(result);
+//     console.log(`your score is ${score}`);
+//     console.log(`your highscore is ${highScore}`);
+//   }
+// };
 
-const gameRound = function (playerChoice, computerChoice) {
+// gamePlay();
+
+//Document script
+let randomNumber = Number(Math.trunc(Math.random() * 6) + 1);
+let score = 0;
+let highScore = 0;
+let tries = 3;
+
+document.querySelector(`.play`).addEventListener(`click`, function () {
+  const playerChoice = document.querySelector(`.input`).textContent;
+  const computerChoice = getComputerChoice();
+  docunent.querySelector(`.computerchoice`).textContent = computerChoice;
+  document.querySelector(`.yourchoice`).textContent = playerChoice;
+
   if (playerChoice === `rock` && computerChoice === `paper`) {
-    return `lose`;
+    document.querySelector(`.result`).textContent = `lose`;
+    tries--;
+    document.querySelector(`.numtries`).textContent = tries;
   } else if (playerChoice === `rock` && computerChoice === `scissor`) {
-    return `win`;
+    document.querySelector(`.result`).textContent = `win`;
+    score++;
+    tries--;
+    document.querySelector(`.dispscore`).textContent = score;
+    if (score > highScore) {
+      highScore = score;
+    }
+    document.querySelector(`.disphighscore`).textContent = highScore;
+    document.querySelector(`.numtries`).textContent = tries;
   } else if (playerChoice === `paper` && computerChoice === `scissor`) {
-    return `lose`;
+    document.querySelector(`.result`).textContent = `lose`; //look here
+    tries--;
+    document.querySelector(`.numtries`).textContent = tries;
   } else if (playerChoice === `paper` && computerChoice === `rock`) {
-    return `win`;
+    document.querySelector(`.result`).textContent = `win`;
+    score++;
+    if (score > highScore) {
+      highScore = score;
+    }
+    tries--;
+    document.querySelector(`.numtries`).textContent = tries;
+    document.querySelector(`.dispscore`).textContent = score;
   } else if (playerChoice === `scissor` && computerChoice === `rock`) {
-    return `lose`;
+    document.querySelector(`.result`).textContent = `lose`;
+    tries--;
+    document.querySelector(`.numtries`).textContent = tries;
   } else if (playerChoice === `scissor` && computerChoice === `paper`) {
-    return `win`;
+    document.querySelector(`.result`).textContent = `win`;
+    score++;
+    if (score > highScore) {
+      highScore = score;
+    }
+    document.querySelector(`.dispscore`).textContent = score;
   } else {
-    return `tie`;
+    document.querySelector(`.result`).textContent = `tie`;
   }
-};
-
-getComputerChoice();
-
-let gameRoundValue = gameRound(playerChoice, computerChoice);
-
-const gamePlay = function () {
-  for (i = 0; i < 5; i++) {
-    console.log(
-      (playerChoice = prompt(`Pick rock paper scissors`).toLowerCase())
-    );
-    randomNumber = Number(Math.trunc(Math.random() * 6) + 1);
-    getComputerChoice();
-    computerChoice = getComputerChoice();
-    gameRoundValue = gameRound(playerChoice, computerChoice);
-    console.log(computerChoice);
-    console.log(gameRoundValue);
-  }
-};
-
-gamePlay();
+});
